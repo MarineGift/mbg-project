79018fc1f84d4177cedbcebb223e03602b211abd
MarineGift <marinegift4u@gmail.com>
2026-05-25 01:47:42 -0500

    stage29c-cleanup: remove temp log files from working tree
    Removes 16 transient debug files generated during launch prep:
    - build_output*.log (6)
    - tsc_*.log/txt (7)
    - regen_*_stderr.log (2)
    - rpc_check.sql (1)
    
    These were tracked previously, added to .gitignore in 026701c.
    Working tree clean for Track B (email-templates) work.

--- rpc_check.sql
+++
@@ -1,6 +0,0 @@
-﻿SELECT n.nspname AS schema, p.proname AS name,
-       pg_get_function_arguments(p.oid) AS args
-FROM pg_proc p
-JOIN pg_namespace n ON n.oid = p.pronamespace
-WHERE p.proname IN ('create_email_tracking','list_email_whitelist','get_unregistered_party_domains','add_email_whitelist','toggle_email_whitelist','delete_email_whitelist','get_party_communications_timeline','get_contact_communications_timeline','get_communications_stats_per_party','get_thread_context','list_templates_for_compose','get_email_history','count_email_history','list_sequences','get_sequence_with_steps','get_party_enrollments','get_tracking_for_communications','get_tracking_for_drafts','get_lead_scores_many')
-ORDER BY schema, name;
